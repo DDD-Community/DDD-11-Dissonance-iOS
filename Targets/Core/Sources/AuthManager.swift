ebbddd58d1eca23c16a3aeba451b3e6ce5360c47
Hansangjin98 <onetable98@naver.com>
2024-07-27 23:49:15 +0900

    feat: #3 - 코어 모듈에 AuthManager, AppProperties 열거형 추가

---
+++ Targets/Core/Sources/AuthManager.swift
@@ -0,0 +1,77 @@
+//
+//  AuthManager.swift
+//  Core
+//
+//  Created by 한상진 on 2024/07/27.
+//  Copyright © 2024 MOZIP. All rights reserved.
+//
+
+import Foundation
+import Security
+
+public enum AuthManager {
+
+  // MARK: - Properties
+  public enum AuthInfoType: String {
+    case accessToken = "accessToken"
+    case refreshToken = "refreshToken"
+    case isFirstLogin = "isFirstLogin"
+  }
+
+  // MARK: - Methods
+  public static func save(authInfoType: AuthInfoType, data: Data) {
+    let query: NSDictionary = .init(
+      dictionary: [
+        kSecClass: kSecClassGenericPassword,
+        kSecAttrAccount: authInfoType.rawValue,
+        kSecValueData: data
+      ]
+    )
+
+    SecItemDelete(query)
+    SecItemAdd(query, nil)
+  }
+
+  public static func load(authInfoType: AuthInfoType) -> Data? {
+    let query: NSDictionary = .init(
+      dictionary: [
+        kSecClass: kSecClassGenericPassword,
+        kSecAttrAccount: authInfoType.rawValue,
+        kSecReturnData: true,
+        kSecMatchLimit: kSecMatchLimitOne
+      ]
+    )
+
+    var dataTypeReference: AnyObject?
+    let status = withUnsafeMutablePointer(to: &dataTypeReference) {
+      SecItemCopyMatching(query, UnsafeMutablePointer($0))
+    }
+
+    guard status == errSecSuccess,
+          let data = dataTypeReference as? Data else {
+      return nil
+    }
+
+    return data
+  }
+
+  public static func deleteTokens() {
+    [AuthInfoType.accessToken, AuthInfoType.refreshToken].forEach {
+      let query: NSDictionary = .init(dictionary: [
+        kSecClass: kSecClassGenericPassword,
+        kSecAttrAccount: $0.rawValue
+      ])
+
+      SecItemDelete(query)
+    }
+  }
+
+  public static func deleteLoginLog() {
+    let query: NSDictionary = .init(dictionary: [
+      kSecClass: kSecClassGenericPassword,
+      kSecAttrAccount: "isFirstLogin"
+    ])
+
+    SecItemDelete(query)
+  }
+}
