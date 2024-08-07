3f57562e00c86df145fa47e8bfb7c85b5e58ee0a
wongbingg <wb277@naver.com>
2024-08-07 17:37:05 +0900

    [ITDS-29] feat: #6 - PostHeader 구현
    - 섹션헤더뷰 구현
    - 더보기 버튼 노출을 위해 public 처리

---
+++ Targets/DesignSystem/Sources/Component/PostHeader.swift
@@ -0,0 +1,77 @@
+//
+//  PostHeader.swift
+//  DesignSystem
+//
+//  Created by 이원빈 on 8/4/24.
+//  Copyright © 2024 MOZIP. All rights reserved.
+//
+
+import UIKit
+
+import PinLayout
+import FlexLayout
+
+public final class PostHeader: UICollectionReusableView {
+  
+  private enum Metric {
+    static let height: CGFloat = 20
+  }
+  
+  // MARK: - UI
+  private let rootFlexContainer = UIView()
+  private let sectionTitle = MozipLabel(style: .heading1, color: MozipColor.gray800)
+  
+  public let showMoreButton = UIButton(type: .custom)
+  private let buttonLabel = MozipLabel(style: .body2, color: MozipColor.gray700, text: "더보기")
+  private let buttonImageView: UIImageView = {
+    let imgView = UIImageView()
+    imgView.contentMode = .scaleAspectFit
+    imgView.image = UIImage(systemName: "chevron.right") // FIXME: 아이콘 변경
+    imgView.tintColor = MozipColor.gray700
+    return imgView
+  }()
+  
+  // MARK: - Initializers
+  public override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupViewHierarchy()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Overrides
+  public override func layoutSubviews() {
+    super.layoutSubviews()
+    setupLayout()
+  }
+  
+//  public override func prepareForReuse() {
+//    super.prepareForReuse()
+//  }
+  
+  // MARK: - Methods
+  public func setTitle(_ text: String) {
+    sectionTitle.updateTextKeepingAttributes(text)
+    sectionTitle.flex.markDirty()
+  }
+  
+  private func setupViewHierarchy() {
+    addSubview(rootFlexContainer)
+    
+    rootFlexContainer.flex.direction(.row).height(Metric.height).define { flex in
+      flex.addItem(sectionTitle)
+      flex.addItem().grow(1)
+      flex.addItem(showMoreButton).direction(.row).alignItems(.center).justifyContent(.end).define { flex in
+        flex.addItem(buttonLabel)
+        flex.addItem(buttonImageView).size(16)
+      }
+    }
+  }
+  
+  private func setupLayout() {
+    rootFlexContainer.pin.all()
+    rootFlexContainer.flex.layout()
+  }
+}
