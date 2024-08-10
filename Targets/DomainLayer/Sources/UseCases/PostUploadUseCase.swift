65a3fc803428aa6bc216239c26ed44cf149acba5
Hansangjin98 <onetable98@naver.com>
2024-08-10 21:57:43 +0900

    feat: #7 - 공고 업로드 기능 도메인 계층 구현

---
+++ Targets/DomainLayer/Sources/UseCases/PostUploadUseCase.swift
@@ -0,0 +1,33 @@
+//
+//  PostUploadUseCase.swift
+//  DomainLayer
+//
+//  Created by 한상진 on 2024/08/10.
+//  Copyright © 2024 MOZIP. All rights reserved.
+//
+
+import RxSwift
+
+public protocol PostUploadUseCaseType {
+  var postUploadRepository: PostUploadRepositoryType { get }
+  
+  //TODO: API 문서 전달받은 후 수정 예정
+  func uploadPost(_ post: Post) -> Observable<Bool>
+}
+
+final class PostUploadUseCase: PostUploadUseCaseType {
+  
+  // MARK: - Properties
+  let postUploadRepository: PostUploadRepositoryType
+  
+  // MARK: - Initializer
+  init(postUploadRepository: PostUploadRepositoryType) {
+    self.postUploadRepository = postUploadRepository
+  }
+  
+  // MARK: - Methods
+  //TODO: API 문서 전달받은 후 수정 예정
+  func uploadPost(_ post: Post) -> Observable<Bool> {
+    return .just(true)
+  }
+}
