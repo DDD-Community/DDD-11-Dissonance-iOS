e2f574f46db39ada99a2f86c36c4d4e7025c7ba0
Hansangjin98 <onetable98@naver.com>
2024-08-14 03:42:42 +0900

    feat: #7 - MozipTextField 구현

---
+++ Targets/DesignSystem/Sources/Component/MozipTextField.swift
@@ -0,0 +1,70 @@
+//
+//  MozipTextField.swift
+//  DesignSystem
+//
+//  Created by 한상진 on 2024/08/11.
+//  Copyright © 2024 MOZIP. All rights reserved.
+//
+
+import UIKit
+
+import RxSwift
+
+public final class MozipTextField: UITextField {
+  
+  // MARK: - Properties
+  private let disposeBag: DisposeBag = .init()
+  
+  // MARK: - Initializer
+  public init(placeHolder: String = .init()) {
+    super.init(frame: .zero)
+    
+    applyPlaceHolder(placeHolder)
+    setupView()
+    bind()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Methods
+  internal func applyPlaceHolder(_ placeHolder: String) {
+    attributedPlaceholder = NSAttributedString(
+      string: placeHolder,
+      attributes: [
+        NSAttributedString.Key.foregroundColor : MozipColor.gray200
+      ]
+    )
+  }
+}
+
+// MARK: - Private Extenion
+private extension MozipTextField {
+  func setupView() {
+    textColor = MozipColor.gray700
+    layer.borderWidth = 1
+    layer.borderColor = MozipColor.gray200.cgColor
+    layer.cornerRadius = 8
+    leftViewMode = .always
+    leftView = .init(frame: .init(x: 0, y: 0, width: 16, height: 1))
+    autocorrectionType = .no
+    spellCheckingType = .no
+  }
+  
+  func bind() {
+    rx.controlEvent(.editingDidBegin)
+      .asSignal()
+      .emit(with: self, onNext: { owner, _ in
+        owner.layer.borderColor = MozipColor.gray700.cgColor
+      })
+      .disposed(by: disposeBag)
+    
+    rx.controlEvent(.editingDidEnd)
+      .asSignal()
+      .emit(with: self, onNext: { owner, _ in
+        owner.layer.borderColor = MozipColor.gray200.cgColor
+      })
+      .disposed(by: disposeBag)
+  }
+}
