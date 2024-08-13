c5935134b7ec12136a8808d7ba9813f633f1773a
Hansangjin98 <onetable98@naver.com>
2024-08-14 06:04:49 +0900

    feat: #7 - MozipImage 열거형 구현, MozipColor 경로 및 파일명 수정

---
+++ Targets/DesignSystem/Sources/Assets/MozipImage.swift
@@ -0,0 +1,14 @@
+//
+//  MozipImage.swift
+//  DesignSystem
+//
+//  Created by 한상진 on 2024/08/14.
+//  Copyright © 2024 MOZIP. All rights reserved.
+//
+
+import UIKit
+
+public enum MozipImage {
+  public static let dropDownIcon = UIImage(named: "dropDownIcon")
+  public static let dropDownActiveIcon = UIImage(named: "dropDownActiveIcon")
+}
