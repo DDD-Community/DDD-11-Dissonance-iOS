8ec7e8766b342f75a2c7903e2be641c4657bbc42
Hansangjin98 <onetable98@naver.com>
2024-09-02 17:20:50 +0900

    feat: #17 - MyPageReactor 구현

---
+++ Targets/PresentationLayer/Sources/MyPage/Reactor/MyPageReactor.swift
@@ -0,0 +1,75 @@
+//
+//  MyPageReactor.swift
+//  PresentationLayer
+//
+//  Created by 한상진 on 2024/08/30.
+//  Copyright © 2024 MOZIP. All rights reserved.
+//
+
+import DomainLayer
+
+import ReactorKit
+
+final class MyPageReactor: Reactor {
+  
+  // MARK: - Properties
+  var initialState: State = .init()
+  
+  // TODO: 추후 구현
+  //  private let useCase: useCaseType
+  
+  enum TableViewSections {
+    case profile, services
+    
+    var headerTitle: String {
+      switch self {
+      case .profile: "내 정보"
+      case .services: "서비스 지원"
+      }
+    }
+    
+    var itemsTitle: [String] {
+      switch self {
+      case .profile: ["연결된 계정"]
+      case .services: ["문의사항", "약관 및 정책", "버전 정보", "로그아웃", "탈퇴하기"]
+      }
+    }
+  }
+  
+  enum Action {
+    case didTapLogoutButton
+    case didTapDeleteAccountButton
+  }
+  
+  enum Mutation {
+    case setLogoutState(Bool)
+  }
+  
+  struct State {
+    var isLoggedOut: Bool = false
+  }
+  
+  // MARK: - Initializer
+  // TODO: 추후 구현
+  init() { }
+  
+  // MARK: - Methods
+  func mutate(action: Action) -> Observable<Mutation> {
+    // TODO: 추후 구현
+    switch action {
+    case .didTapLogoutButton:        return .just(.setLogoutState(true))
+    case .didTapDeleteAccountButton: return .just(.setLogoutState(true))
+    }
+  }
+  
+  func reduce(state: State, mutation: Mutation) -> State {
+    var newState = state
+    
+    switch mutation {
+    case .setLogoutState(let isLoggedOut):
+      newState.isLoggedOut = isLoggedOut
+    }
+    
+    return newState
+  }
+}
