bf941306e6c6140784e2b90f5e97cbe836cf6373
wongbingg <wb277@naver.com>
2024-09-04 20:36:38 +0900

    fix: appleLogin 리스폰스값 JWT로 정확히 수정 반영

--- Targets/DataLayer/Sources/Repositories/LoginRepository.swift
+++ Targets/DataLayer/Sources/Repositories/LoginRepository.swift
@@ -70,8 +70,11 @@ private extension LoginRepository {
   func parseJWT(from authorization: ASAuthorization) -> String {
     switch authorization.credential {
     case let appleIDCredential as ASAuthorizationAppleIDCredential:
-      let userIdentifier = appleIDCredential.user // jwt 값
-      return userIdentifier
+      guard let jwtData = appleIDCredential.identityToken,
+            let jwtString = String(data: jwtData, encoding: .utf8) else {
+        return "NULL"
+      }
+      return jwtString
     case let passwordCredential as ASPasswordCredential:
       let userIdentifier = passwordCredential.user
       return userIdentifier
