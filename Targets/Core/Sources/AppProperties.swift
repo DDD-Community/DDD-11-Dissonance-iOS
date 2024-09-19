de9259e5ef3e46a778186f7545daf35a4df58a5d
Hansangjin98 <onetable98@naver.com>
2024-09-19 23:49:59 +0900

    feature: #24 - Core모듈 AppProperties에서 baseURL 수정, accessToken 추가

--- Targets/Core/Sources/AppProperties.swift
+++ Targets/Core/Sources/AppProperties.swift
@@ -13,6 +13,12 @@ public struct AppProperties {
   public static var baseURL: String {
     guard let infoDictionary = Bundle.main.infoDictionary,
           let baseURL = infoDictionary["BASE_URL"] as? String else { return .init() }
-    return "http://\(baseURL)"
+    return "https://\(baseURL)"
+  }
+  
+  public static var accessToken: String {
+    guard let accessTokenData = AuthManager.load(authInfoType: .accessToken) else { return .init() }
+    
+    return "Bearer \(String(decoding: accessTokenData, as: UTF8.self))"
   }
 }
