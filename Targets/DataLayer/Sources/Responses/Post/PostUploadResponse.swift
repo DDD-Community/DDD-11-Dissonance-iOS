091b4777253b37dcb068090775b0513ebafe69f8
Hansangjin98 <onetable98@naver.com>
2024-09-20 00:05:41 +0900

    feature: #24 - 공고 업로드 Response 구현

---
+++ Targets/DataLayer/Sources/Responses/Post/PostUploadResponse.swift
@@ -0,0 +1,12 @@
+//
+//  PostUploadResponse.swift
+//  DataLayer
+//
+//  Created by 한상진 on 2024/09/14.
+//  Copyright © 2024 MOZIP. All rights reserved.
+//
+
+public struct PostUploadResponse: Decodable {
+  let id: Int
+  let title: String
+}
