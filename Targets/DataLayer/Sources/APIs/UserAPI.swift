2c56abafff495c00162e689b3c66ca9f67c815ec
Hansangjin98 <onetable98@naver.com>
2024-09-23 03:00:12 +0900

    feat: #28 - UserAPI 열거형 구현

---
+++ Targets/DataLayer/Sources/APIs/UserAPI.swift
@@ -0,0 +1,77 @@
+//
+//  UserAPI.swift
+//  DataLayer
+//
+//  Created by 한상진 on 2024/09/23.
+//  Copyright © 2024 MOZIP. All rights reserved.
+//
+
+import Core
+import Foundation
+
+import Moya
+
+enum UserAPI {
+  case information
+  case regenerate
+  case delete
+  case logout
+}
+
+// MARK: - TargetType
+extension UserAPI: TargetType {
+  var baseURL: URL {
+    URL(string: AppProperties.baseURL)!
+  }
+
+  var path: String {
+    let basePath = "/users"
+
+    switch self {
+    case .information: return basePath
+    case .regenerate: return basePath + "/reissue"
+    case .delete: return basePath
+    case .logout: return basePath + "/logout"
+    }
+  }
+
+  var method: Moya.Method {
+    switch self {
+    case .information: return .get
+    case .regenerate: return .post
+    case .delete: return .delete
+    case .logout: return .get
+    }
+  }
+
+  public var sampleData: Data {
+    return .init()
+  }
+
+  public var headers: [String : String]? {
+    ["Authorization": "Bearer \(AppProperties.accessToken)"]
+  }
+
+  public var task: Task {
+    let requestParam: [String: Any] = parameters ?? [:]
+    let encoding: ParameterEncoding
+
+    switch self.method {
+    case .post, .patch, .put, .delete:
+      encoding = JSONEncoding.default
+
+    default:
+      encoding = URLEncoding.default
+    }
+
+    return .requestParameters(parameters: requestParam, encoding: encoding)
+  }
+
+  private var parameters: [String: Any]? {
+    switch self {
+    case .regenerate: return ["refreshToken": AppProperties.refreshToken]
+    case .logout: return ["Authorization": AppProperties.accessToken]
+    default: return nil
+    }
+  }
+}
