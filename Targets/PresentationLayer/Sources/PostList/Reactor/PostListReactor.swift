4fdefd494fa11931ee0906a3692ff97124c054d3
wongbingg <wb277@naver.com>
2024-10-15 21:33:08 +0900

    [ITDS-49] refactor: #33 PostListReactor 코드정리
    - TODO: 커스텀 에러케이스 정의가 완료되면, 에러핸들링 메서드 적용필요

--- Targets/PresentationLayer/Sources/PostList/Reactor/PostListReactor.swift
+++ Targets/PresentationLayer/Sources/PostList/Reactor/PostListReactor.swift
@@ -31,10 +31,9 @@ final class PostListReactor: Reactor {
   }
 
   enum Mutation {
-    case setLoading
+    case setLoading(Bool)
     case setPosts(data: [PostCellData])
-    case setSelectedCell(data: PostCellData)
-    case deleteSelectedCell
+    case setSelectedCell(data: PostCellData?)
   }
 
   struct State {
@@ -47,41 +46,45 @@ final class PostListReactor: Reactor {
   // MARK: - Methods
   func mutate(action: Action) -> Observable<Mutation> {
     switch action {
-    case let .fetchPosts(id, order):
-      return .concat([
-        .just(.setLoading),
-        fetchPostListUseCase.execute(
-          categoryId: id,
-          pageable: .init(page: 0, size: 30, sort: order.rawValue) // FIXME: 추후 페이징처리
-        )
-        .map { .setPosts(data: $0) }
-      ])
-    case let .tapCell(indexPath):
-      return fetchCellData(at: indexPath)
-        .flatMap { a -> Observable<Mutation> in // FIXME: 추후 정리
-          Observable.concat([
-            .just(.setSelectedCell(data: a)),
-            .just(.deleteSelectedCell)
-          ])
-        }
+    case let .fetchPosts(id, order): return fetchPosts(id: id, order: order)
+    case let .tapCell(indexPath):    return tapCell(at: indexPath)
     }
   }
 
   func reduce(state: State, mutation: Mutation) -> State {
     var newState = state
     switch mutation {
-    case .setLoading:
-      newState.isLoading = true
-      newState.selectedCell = nil
-    case let .setPosts(data):
-      newState.posts = data
-      newState.isLoading = false
-    case let .setSelectedCell(data):
-      newState.selectedCell = data
-    case .deleteSelectedCell: // FIXME: 추후 정리
-      newState.selectedCell = nil
+    case let .setLoading(bool):      newState.isLoading = bool
+    case let .setPosts(data):        newState.posts = data
+    case let .setSelectedCell(data): newState.selectedCell = data
     }
     return newState
+  }
+  
+  private func fetchPosts(id: Int, order: PostOrder) -> Observable<Mutation> {
+    let fetchPostListMutation: Observable<Mutation> = fetchPostListUseCase
+      .execute(
+        categoryId: id,
+        pageable: .init(page: 0, size: 30, sort: order.rawValue) // FIXME: 추후 페이징처리
+      )
+      .map { .setPosts(data: $0) }
+      // TODO: .catch { error in ... } 에러처리 필요.
+    let sequence: [Observable<Mutation>] = [
+      .just(.setLoading(true)),
+      fetchPostListMutation,
+      .just(.setLoading(false))
+    ]
+    return .concat(sequence)
+  }
+  
+  private func tapCell(at indexPath: IndexPath) -> Observable<Mutation> {
+    return fetchCellData(at: indexPath)
+      .flatMap { cellData -> Observable<Mutation> in
+        Observable.concat([
+          .just(.setSelectedCell(data: cellData)),
+          .just(.setSelectedCell(data: nil))
+        ])
+      }
   }
   
   private func fetchCellData(at indexPath: IndexPath) -> Observable<PostCellData> {
