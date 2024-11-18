77b878431657ab67e9c4a04993356022e6481ee4
Hansangjin98 <onetable98@naver.com>
2024-11-18 17:20:17 +0900

    feat: #40 - 공고 업로드 ViewController에 공고 수정 관련 로직 적용, 2차 MVP UI 변경사항 적용

--- Targets/PresentationLayer/Sources/PostUpload/View/PostUploadViewController.swift
+++ Targets/PresentationLayer/Sources/PostUpload/View/PostUploadViewController.swift
@@ -52,20 +52,20 @@ final class PostUploadViewController: BaseViewController<PostUploadReactor>, Ale
   
   // MARK: UI 컴포넌트
   private let imageUploadView: ImageUploadView = .init(frame: .zero)
-  private let titleTextFieldView: LabelWithTextFieldView = .init(title: "제목", placeHolder: "공고 제목을 입력해주세요.")
-  private let categoryTextFieldView: LabelWithTextFieldView = .init(title: "공고 카테고리", placeHolder: "공고 카테고리를 선택해주세요.")
+  private let titleTextFieldView: LabelWithTextFieldView = .init(title: "제목", placeHolder: "공고 제목을 입력해주세요.", isRequired: true)
+  private let categoryTextFieldView: LabelWithTextFieldView = .init(title: "공고 카테고리", placeHolder: "공고 카테고리를 선택해주세요.", isRequired: true)
   private let dropDownIcon: UIImageView = .init(image: DesignSystemAsset.dropDownIcon.image)
   private let categoryView: PostCategoryView = .init()
-  private let organizationTextFieldView: LabelWithTextFieldView = .init(title: "기관•단체", placeHolder: "기관•단체 이름을 입력해주세요.")
+  private let organizationTextFieldView: LabelWithTextFieldView = .init(title: "기관•단체", placeHolder: "기관•단체 이름을 입력해주세요.", isRequired: true)
   private let recruitStartTextFieldView: LabelWithTextFieldView = .init(title: "모집 시작 일자", placeHolder: "모집 시작 일자를 정해주세요.")
-  private let recruitEndTextFieldView: LabelWithTextFieldView = .init(title: "모집 마감 일자", placeHolder: "모집 마감 일자를 정해주세요.")
+  private let recruitEndTextFieldView: LabelWithTextFieldView = .init(title: "모집 마감 일자", placeHolder: "모집 마감 일자를 정해주세요.", isRequired: true)
   private let recruitJobView: RecruitJobGroupView = .init()
   private let activityStartTextFieldView: LabelWithTextFieldView = .init(title: "활동 시작 일자", placeHolder: "활동 시작 일자를 정해주세요.")
   private let activityEndTextFieldView: LabelWithTextFieldView = .init(title: "활동 종료 일자", placeHolder: "활동 종료 일자를 정해주세요.")
-  private let activityContentsTextView: LabelWithTextView = .init(title: "활동 내용", placeHolder: "자세한 내용을 입력해주세요.")
-  private let postUrlTextFieldView: LabelWithTextFieldView = .init(title: "공고 URL", placeHolder: "공고 URL 링크를 입력해주세요.")
+  private let activityContentsTextView: LabelWithTextView = .init(title: "활동 내용", placeHolder: "자세한 내용을 입력해주세요.", isRequired: true)
+  private let postUrlTextFieldView: LabelWithTextFieldView = .init(title: "공고 URL", placeHolder: "공고 URL 링크를 입력해주세요.", isRequired: true)
   private let bottomShadowView: BottomShadowView = .init()
-  private let completionButton: RectangleButton = .init(title: "등록 완료", fontStyle: .heading1, titleColor: .white, backgroundColor: MozipColor.gray200)
+  private let completionButton: RectangleButton = .init(fontStyle: .heading1, titleColor: .white, backgroundColor: MozipColor.gray200)
   
   // MARK: - Initializer
   init(reactor: PostUploadReactor) {
@@ -90,6 +90,8 @@ final class PostUploadViewController: BaseViewController<PostUploadReactor>, Ale
     ].forEach {
       setupDatePicker($0)
     }
+    
+    setupOriginPost()
   }
   
   override func viewWillAppear(_ animated: Bool) {
@@ -132,6 +134,7 @@ final class PostUploadViewController: BaseViewController<PostUploadReactor>, Ale
     scrollView.addSubview(rootContainer)
     categoryTextFieldView.textField.addSubview(dropDownIcon)
     view.addSubview(bottomShadowView)
+    completionButton.updateTitle((reactor?.post.title.isEmpty ?? true) ? "등록 완료" : "수정 완료")
     bottomShadowView.addSubview(completionButton)
     
     rootContainer.flex
@@ -176,53 +179,107 @@ private extension PostUploadViewController {
   }
   
   // MARK: Methods
+  func setupOriginPost() {
+    guard let originPost = reactor?.post, !originPost.title.isEmpty else { return }
+    
+    let originPostValues = [
+      originPost.title,
+      originPost.category?.title ?? originPost.categoryTitle,
+      originPost.organization,
+      originPost.recruitStartDate,
+      originPost.recruitEndDate,
+      originPost.activityStartDate,
+      originPost.activityEndDate,
+      originPost.postUrlString
+    ]
+    
+    let textFieldViews: [LabelWithTextFieldView] = [
+      titleTextFieldView,
+      categoryTextFieldView,
+      organizationTextFieldView,
+      recruitStartTextFieldView,
+      recruitEndTextFieldView,
+      activityStartTextFieldView,
+      activityEndTextFieldView,
+      postUrlTextFieldView
+    ]
+    
+    guard originPostValues.count == textFieldViews.count else { return }
+    
+    for i in 0..<originPostValues.count {
+      textFieldViews[i].textField.rx.text.onNext(originPostValues[i])
+    }
+    
+    recruitJobView.setEditMode()
+    originPost.jobGroups.forEach {
+      recruitJobView.makeField(value: $0)
+    }
+    
+    activityContentsTextView.textView.rx.text.onNext(originPost.activityContents)
+    reactor?.action.onNext(.inputImage(originPost.imageData))
+    
+    DispatchQueue.main.async {
+      self.imageUploadView.applyImage(UIImage(data: originPost.imageData) ?? .init())
+    }
+  }
+  
   func bindAction(reactor: PostUploadReactor) {
     titleTextFieldView.textObservable
+      .skip(1)
       .map { Action.inputTitle($0) }
       .bind(to: reactor.action)
       .disposed(by: disposeBag)
     
     categoryTextFieldView.textObservable
+      .skip(1)
       .map { Action.inputCategory($0) }
       .bind(to: reactor.action)
       .disposed(by: disposeBag)
     
     organizationTextFieldView.textObservable
+      .skip(1)
       .map { Action.inputOrganization($0) }
       .bind(to: reactor.action)
       .disposed(by: disposeBag)
     
     recruitStartTextFieldView.textObservable
+      .skip(1)
       .map { Action.inputRecruitStartDate($0) }
       .bind(to: reactor.action)
       .disposed(by: disposeBag)
     
     recruitEndTextFieldView.textObservable
+      .skip(1)
       .map { Action.inputRecruitEndDate($0) }
       .bind(to: reactor.action)
       .disposed(by: disposeBag)
     
     recruitJobView.allValueObservable
+      .skip(1)
       .map { Action.inputJobGroup($0) }
       .bind(to: reactor.action)
       .disposed(by: disposeBag)
     
     activityStartTextFieldView.textObservable
+      .skip(1)
       .map { Action.inputActivityStartDate($0) }
       .bind(to: reactor.action)
       .disposed(by: disposeBag)
     
     activityEndTextFieldView.textObservable
+      .skip(1)
       .map { Action.inputActivityEndDate($0) }
       .bind(to: reactor.action)
       .disposed(by: disposeBag)
     
     activityContentsTextView.textObservable
+      .skip(1)
       .map { Action.inputActivityContents($0) }
       .bind(to: reactor.action)
       .disposed(by: disposeBag)
     
     postUrlTextFieldView.textObservable
+      .skip(1)
       .map { Action.inputPostUrlString($0) }
       .bind(to: reactor.action)
       .disposed(by: disposeBag)
@@ -339,7 +396,7 @@ private extension PostUploadViewController {
   func setupDatePicker(_ textFieldView: LabelWithTextFieldView) {
     let datePicker: UIDatePicker = .init()
     datePicker.datePickerMode = .date
-    datePicker.preferredDatePickerStyle = .wheels
+    datePicker.preferredDatePickerStyle = .inline
     datePicker.locale = Locale(identifier: "ko_KR")
     
     if let backgroundView = datePicker.subviews.first?.subviews.first {
