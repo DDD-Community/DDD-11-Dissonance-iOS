54aa45c77afe47147c4297c92c54f0ac60f24f7f
Hansangjin98 <onetable98@naver.com>
2024-11-18 17:18:49 +0900

    feat: #40 - 공고 상세 Reactor에 공고 삭제 관련 로직 구현

--- Targets/PresentationLayer/Sources/PostDetail/Reactor/PostDetailReactor.swift
+++ Targets/PresentationLayer/Sources/PostDetail/Reactor/PostDetailReactor.swift
@@ -14,12 +14,13 @@ final class PostDetailReactor: Reactor {
   
   // MARK: - Properties
   private let postDetailUseCase: PostDetailUseCaseType
-  private let postID: Int
+  public let postID: Int
   var initialState: State = .init()
   
   enum Action {
     case fetchPost
     case didTapReportButton
+    case didTapDeleteButton
     case didTapImageView
     case dismissImageViewController
   }
@@ -27,6 +28,8 @@ final class PostDetailReactor: Reactor {
   enum Mutation {
     case setPost(Post)
     case setLoading(Bool)
+    case setDeleteState(Bool)
+    case setDeletionError(Bool)
     case setIsPresentFullImage(Bool)
     case setFetchError(Bool)
     case setReportState(Bool)
@@ -36,8 +39,10 @@ final class PostDetailReactor: Reactor {
   struct State {
     var post: Post = .init()
     var isLoading: Bool = false
+    var isDeleted: Bool = false
     var isPresentFullImage: Bool = false
     var isFetchError: Bool = false
+    var isDeletionError: Bool = false
     var isSuccessReport: Bool = false
     var isErrorReport: (isError: Bool, message: String) = (false, "")
   }
@@ -52,6 +57,7 @@ final class PostDetailReactor: Reactor {
   func mutate(action: Action) -> Observable<Mutation> {
     switch action {
     case .fetchPost: return .concat([.just(.setLoading(true)), fetchPost(id: postID), .just(.setLoading(false))])
+    case .didTapDeleteButton: return .concat([.just(.setLoading(true)), deletePost(id: postID), .just(.setLoading(false))])
     case .didTapReportButton: return .concat([.just(.setLoading(true)), reportPost(id: postID), .just(.setLoading(false))])
     case .didTapImageView: return .just(.setIsPresentFullImage(true))
     case .dismissImageViewController: return .just(.setIsPresentFullImage(false))
@@ -65,7 +71,9 @@ final class PostDetailReactor: Reactor {
     switch mutation {
     case .setPost(let post):          newState.post = post
     case .setLoading(let isLoading):  newState.isLoading = isLoading
+    case .setDeleteState(let isDeleted): newState.isDeleted = isDeleted
     case .setIsPresentFullImage(let isPresent): newState.isPresentFullImage = isPresent
+    case .setDeletionError(let isDeletionError): newState.isDeletionError = isDeletionError
     case .setFetchError(let isError): newState.isFetchError = isError
     case .setReportState(let isSuccess): newState.isSuccessReport = isSuccess
     case let .setReportError(isError, message): newState.isErrorReport = (isError: isError, message: message)
@@ -83,6 +91,16 @@ private extension PostDetailReactor {
         switch networkResult {
         case .success(let post): return .just(.setPost(post))
         case .error: return .just(.setFetchError(true))
+        }
+      }
+  }
+  
+  func deletePost(id: Int) -> Observable<Mutation> {
+    postDetailUseCase.deletePost(id: id)
+      .flatMap { networkResult -> Observable<Mutation> in
+        switch networkResult {
+        case .success: return .just(.setDeleteState(true))
+        case .error: return .just(.setDeletionError(true))
         }
       }
   }
