420cf48c5d55de79ed22a645747eb8d991dac532
Hansangjin98 <onetable98@naver.com>
2025-05-16 16:43:06 +0900

    [ITDS-82] feat: #72 - 북마크 토글 기능 관련 도메인 로직 구현

--- Targets/DomainLayer/Sources/DomainAssembly.swift
+++ Targets/DomainLayer/Sources/DomainAssembly.swift
@@ -50,5 +50,9 @@ public struct DomainAssembly: DependencyAssemblable {
     container.register(type: MyPageUseCaseType.self) { container in
       return MyPageUseCase(userRepository: container.resolve(type: UserRepositoryType.self)!)
     }
+    
+    container.register(type: BookmarkToggleUseCaseType.self) { container in
+      return BookmarkToggleUseCase(bookmarkRepository: container.resolve(type: BookmarkRepositoryType.self)!)
+    }
   }
 }
