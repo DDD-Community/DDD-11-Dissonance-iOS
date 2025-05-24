fc258a118d763acac02146c9677f7a03b4fbca32
wongbingg <wb277@naver.com>
2025-05-24 12:16:53 +0900

    [ITDS-85] fix: PostEdit 요청 시, 날짜 데이터 변환 문제 해결
    - 상세 내용은 주석에 적어두었습니다

--- Targets/DomainLayer/Sources/Entities/Post.swift
+++ Targets/DomainLayer/Sources/Entities/Post.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import MozipCore
 
 public struct Post: Equatable {
 
@@ -38,9 +39,26 @@ public struct Post: Equatable {
     return category.ID
   }
   
+  /// 날짜 데이터 4개(활동 시작일, 활동 마감일, 모집 시작일, 모집 마감일)의 포맷은
+  /// Response로 받을 땐 yyyy.MM.dd 형태로 받아짐.
+  /// Request로 보낼 땐 yyyy년 MM월 dd일 형태로 보내야 함.
+  /// PostEdit에서 사용하기 위해 날짜 포맷을 변환하는 메서드
+  public mutating func mapDateValuesToRequestFormat() {
+    recruitStartDate = translateToRequestFormat(for: recruitStartDate)
+    recruitEndDate = translateToRequestFormat(for: recruitEndDate)
+    activityStartDate = translateToRequestFormat(for: activityStartDate)
+    activityEndDate = translateToRequestFormat(for: activityEndDate)
+  }
+  
   public static func == (lhs: Post, rhs: Post) -> Bool {
     lhs.title == rhs.title &&
     lhs.organization == rhs.organization &&
     lhs.postUrlString == rhs.postUrlString
+  }
+  
+  private func translateToRequestFormat(for dateValue: String) -> String {
+    guard !dateValue.isEmpty else { return "" }
+    let date = DateFormatter.yyyyMMdd_ResponseFormat.date(from: dateValue) ?? .init()
+    return DateFormatter.yyyyMMdd_RequestFormat.string(from: date)
   }
 }
